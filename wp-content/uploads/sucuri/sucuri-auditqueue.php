419437acfedaf781142326ded1f00aa0e2fc8103
akkia1 <capmovie761@gmail.com>
2024-12-04 23:36:30 +0100

    test

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,26 @@
+<?php
+// datastore=auditqueue;
+// created_on=1733347028;
+// updated_on=1733347028;
+exit(0);
+?>
+1733347028_9145:"Warning: root, 127.0.0.1; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.9.6; sucuri-scanner\/sucuri.php)"
+1733347100_5124:"Warning: root, 127.0.0.1; Plugin installed: Unknown"
+1733347141_4847:"Warning: root, 127.0.0.1; Plugin activated: Polylang (v3.6.5; polylang\/polylang.php)"
+1733347191_5164:"Notice: root, 127.0.0.1; Category created; ID: 9; name: Uncategorized"
+1733347207_9672:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 316,Old status: new,New status: publish,Title: Front Page - English"
+1733347208_0492:"Notice: root, 127.0.0.1; Page was created; ID: 316; name: Front Page - English"
+1733347361_2252:"Warning: root, 127.0.0.1; Plugin installed: Unknown"
+1733347611_0999:"Warning: root, 127.0.0.1; Plugin activated: W3 Total Cache (v2.8.0; w3-total-cache\/w3-total-cache.php)"
+1733348821_1638:"Warning: root, 127.0.0.1; Plugin installed: Unknown"
+1733349142_6262:"Notice: root, 127.0.0.1; Media file added; ID: 317; name: woocommerce-placeholder; type: image\/png"
+1733349147_2778:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 318,Old status: new,New status: publish,Title: Shop"
+1733349147_2812:"Notice: root, 127.0.0.1; Page was created; ID: 318; name: Shop"
+1733349147_8219:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 319,Old status: new,New status: publish,Title: Cart"
+1733349147_8239:"Notice: root, 127.0.0.1; Page was created; ID: 319; name: Cart"
+1733349147_9846:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 320,Old status: new,New status: publish,Title: Checkout"
+1733349147_9883:"Notice: root, 127.0.0.1; Page was created; ID: 320; name: Checkout"
+1733349148_1049:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 321,Old status: new,New status: publish,Title: My account"
+1733349148_1073:"Notice: root, 127.0.0.1; Page was created; ID: 321; name: My account"
+1733349148_2202:"Notice: root, 127.0.0.1; Page status has been changed; details: ID: 322,Old status: new,New status: draft,Title: Refund and Returns Policy"
+1733349149_0979:"Warning: root, 127.0.0.1; Plugin activated: WooCommerce (v9.4.3; woocommerce\/woocommerce.php)"
---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1733347058;
+// updated_on=1733347058;
+exit(0);
+?>
